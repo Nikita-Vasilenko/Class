b05e3234fe54f9d83e326ea44c82a73ce925ef7b
Nikita-Vasilenko <73024299+Nikita-Vasilenko@users.noreply.github.com>
2021-01-19 12:28:27 +0300

    Add files via upload

---
+++ Chicla.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Chicla
+    {
+        private double materialone;
+        private double materialtwo;
+        public Chicla(double materialone, double materialtwo)
+        {
+            this.materialone = materialone;
+            this.materialtwo = materialtwo;
+        }
+        private double Sum()
+        {
+            return materialone * materialone + materialtwo * materialtwo;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Число а:"+ materialone+"Число b:"+ materialtwo+"Сумма:"+Sum());
+        }
+    }
+}
