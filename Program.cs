5842c4380d05fff52573f7daaeb8b4ae960e573d
Nikita-Vasilenko <73024299+Nikita-Vasilenko@users.noreply.github.com>
2021-01-19 12:35:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите число b:");
+            double b = double.Parse(Console.ReadLine());
+            Chicla chicla = new Chicla(a, b);
+            chicla.Print();
+            Console.ReadKey();
+        }
+    }
+}
